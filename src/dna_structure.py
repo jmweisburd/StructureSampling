a2a9e6b623df1ff4e2f231c51215612d0e02ddba
Julian Weisburd <jmweisburd@gmail.com>
2017-08-10 10:28:30 -0600

    Fixed small bug

--- src/dna_structure.py
+++ src/dna_structure.py
@@ -2,7 +2,7 @@ from coords import *
 import random
 from constants import DS_LENGTH, SS_LENGTH
 import options
-from wlc import
+from wlc import *
 
 class DNA_Structure:
     def __init__(self, cart_coords, joint_list, domain_list):
@@ -36,14 +36,14 @@ class DNA_Structure:
             if j.id == 0:
                 j.cart_coords = (self.tether_location)
                 next_domain = self.get_domain_by_id(j.up_domain)
-                radial = next_domain.get_dna_length_nm()
+                radial = next_domain.get_dna_length_nm(worm)
                 new_vector = random_vector_generation_pz(radial)
                 next_joint = self.get_joint_by_id(next_domain.up_joint)
                 next_joint.cart_coords = add_3d_vectors(j.cart_coords, new_vector)
             else:
                 current_vector = j.cart_coords
                 next_domain = self.get_domain_by_id(j.up_domain)
-                radial = next_domain.get_dna_length_nm()
+                radial = next_domain.get_dna_length_nm(worm)
                 new_vector = random_vector_generation(radial)
                 while (current_vector.z + new_vector.z) < 0:
                     new_vector = random_vector_generation(radial)
@@ -58,14 +58,6 @@ class DNA_Structure:
         for d in self.domain_list:
             print(d.id, d.stranded, d.down_joint, d.up_joint, d.length)
 
-    def get_xs_zs(self):
-        xs = []
-        zs = []
-        for j in self.joint_list:
-            xs.append(j.cart_coords[0])
-            zs.append(j.cart_coords[2])
-        return xs, zs
-
     def get_last_joint_coords(self):
         last_j = self.joint_list[len(self.joint_list)-1]
         return last_j.cart_coords
@@ -75,16 +67,6 @@ class DNA_Structure:
         j1, j2 = self.joint_list[len(self.joint_list)-2], self.joint_list[len(self.joint_list)-1]
         xm, ym, zm = (j1.cart_coords.x + j2.cart_coords.x)/2, (j1.cart_coords.y + j2.cart_coords.y)/2, (j1.cart_coords.z + j2.cart_coords.z)/2
         return CartesianCoords(xm, ym, zm)
-
-    def get_joint_z_differences(self):
-        z_diff = []
-        for i in range(0, len(self.joint_list)-1):
-            j = self.joint_list[i]
-            d_next = self.get_domain_by_id(j.up_domain)
-            j_next = self.get_joint_by_id(d_next.up_joint)
-            diff = j_next.cart_coords[2] - j.cart_coords[2]
-            z_diff.append(diff)
-        return z_diff
 
 class Joint:
     def __init__(self, j_id, dd, ud):
@@ -101,12 +83,12 @@ class Domain:
         self.up_joint = up
         self.length = float(l)
 
-    def get_dna_length_nm(self):
+    def get_dna_length_nm(self, worm):
         if self.stranded == "D":
             return self.length*DS_LENGTH
         else:
             max_length = self.length*SS_LENGTH
-            if self.wc:
+            if worm:
                 return get_a_wlc_sample(2, max_length)
             else:
                 return random.uniform(0, max_length)
