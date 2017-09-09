3071e93c00048e55b29267b7efa4f2aee3fd6f4a
Julian Weisburd <jmweisburd@gmail.com>
2017-09-09 00:07:01 -0600

    Running LC calculations for all 4 distribution combinations

--- analysis/new_lc.py
+++ analysis/new_lc.py
@@ -7,34 +7,38 @@ base = os.path.normpath(os.getcwd() + os.sep + os.pardir)
 y_ds = [10.88]
 #bins = [0.5,1.0,1.5,2.0,2.5,3.0,3.5,4.0]
 bins = [0.5,1.0,1.5,2.0]
-distr = ['uni', 'wc']
+#distr = ['uni', 'wc']
 
-with open('fixed_lc.txt', 'w') as f:
-    for d in distr:
-        f.write("### " + d + " ###\n")
+dist = 'uni_uni'
+#dist = 'uni_nicked'
+#dist = 'worm_uni'
+#dist = 'worm_nicked'
+
+with open(dist, 'w') as f:
+    f.write("### " + dist + " ###\n")
+    f.write("\n")
+    for y in y_ds:
+        f.write("### TOEHOLD DISTANCE " + str(y) + " nm ###\n")
         f.write("\n")
-        for y in y_ds:
-            f.write("### TOEHOLD DISTANCE " + str(y) + " nm ###\n")
+
+        long_path = base + "/data/" + dist + "/" + str(y) + "/long.txt"
+        short_path = base + "/data/" + dist + "/" + str(y) + "/short.txt"
+
+        for b in bins:
+            print("DISTRIBUTION " + str(dist))
+            print("DISTANCE" + str(y))
+            print("BIN SIZE " + str(b))
+            reactive_vol = (4/3) * math.pi * pow(b, 3)
+            pc = ProbabilityCalculator()
+            pc.set_bw(b)
+            pc.read_files_to_maps(long_path, short_path)
+
+            f.write("### BIN/THRESHOLD VALUE: " + str(b) + " nm ###\n")
             f.write("\n")
 
-            long_path = base + "/data/" + d + "/" + str(y) + "/long.txt"
-            short_path = base + "/data/" + d + "/" + str(y) + "/short.txt"
-
-            for b in bins:
-                print("DISTRIBUTION " + str(d))
-                print("DISTANCE" + str(y))
-                print("BIN SIZE " + str(b))
-                reactive_vol = (4/3) * math.pi * pow(b, 3)
-                pc = ProbabilityCalculator()
-                pc.set_bw(b)
-                pc.read_files_to_maps(long_path, short_path)
-
-                f.write("### BIN/THRESHOLD VALUE: " + str(b) + " nm ###\n")
-                f.write("\n")
-
-                lc = pc.new_local_conc()
-                f.write("VOL: " + str(reactive_vol) + "\n")
-                f.write("LC: " + str(lc) + "\n")
-                f.write("\n")
+            lc = pc.new_local_conc()
+            f.write("VOL: " + str(reactive_vol) + "\n")
+            f.write("LC: " + str(lc) + "\n")
+            f.write("\n")
 
 f.close()
